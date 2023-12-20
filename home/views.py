3db6ec24f82c8bb1d26c1c19e8fc2da3561aa53b
GenaPlem <shainkill1337@gmail.com>
2023-12-20 11:43:15 +0000

    Fix home view

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from products.models import Product
 def home(request):
     """ A view to return the index page """
     top_products = Product.objects.filter(is_top_product=True)
-    return render(request, 'home.html', {'top_products': top_products})
+    return render(request, 'home/index.html', {'top_products': top_products})
