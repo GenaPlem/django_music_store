bb9bcfa51d27f8e49c10cca45c4785e6aedf8e24
GenaPlem <shainkill1337@gmail.com>
2023-12-21 10:57:59 +0000

    Add pagination to the products page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-
 from .models import Product, Category
 
 
@@ -12,10 +12,21 @@ def all_products(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
+    paginator = Paginator(products, 1)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
     context = {
         'category': category,
         'categories': categories,
         'products': products,
+        'page': page,
     }
 
     return render(request, 'products/products.html', context)
