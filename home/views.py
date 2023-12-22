cf0ea7f6f5e6801c0be46c2e4b70723fd11822d3
GenaPlem <shainkill1337@gmail.com>
2023-12-22 13:40:24 +0000

    Update top products section on the homepage

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from products.models import Product
 
 def home(request):
     """ A view to return the index page """
-    top_products = Product.objects.filter(is_top_product=True)
+    top_products = Product.objects.filter(is_top_product=True).order_by('?')[:3]
     return render(request, 'home/index.html', {'top_products': top_products})
