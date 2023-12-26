0627c86c0e803198dca31fba498c644483f24bce
GenaPlem <shainkill1337@gmail.com>
2023-12-26 11:07:38 +0000

    Add second font

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from products.models import Product
 
 def home(request):
     """ A view to return the index page """
-    top_products = Product.objects.filter(is_top_product=True)[:3]
+    top_products = Product.objects.filter(is_top_product=True)[:6]
     return render(request, 'home/index.html', {'top_products': top_products})
