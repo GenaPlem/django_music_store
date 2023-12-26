c61d2fff75a3fb8b452e711af1030bfb0efac4b1
GenaPlem <shainkill1337@gmail.com>
2023-12-26 11:56:20 +0000

    Add views for orders and shipping info

--- profiles/views.py
+++ profiles/views.py
@@ -5,3 +5,13 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     return render(request, 'profiles/profile.html')
+
+
+@login_required
+def orders(request):
+    return render(request, 'profiles/orders.html')
+
+
+@login_required
+def shipping_info(request):
+    return render(request, 'profiles/shipping_info.html')
