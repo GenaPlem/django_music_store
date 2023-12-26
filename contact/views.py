5393721342c03f7f7903aa4fc9aef57f19c88ff9
GenaPlem <shainkill1337@gmail.com>
2023-12-26 17:49:32 +0000

    Add sending mail with data from contact form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import ContactForm
-from .models import Contact
 
 
 def contact(request):
@@ -9,6 +10,13 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             contact_form = form.save()
+
+            send_mail(
+                subject=f"New contact inquiry from {contact_form.name}",
+                message=contact_form.message,
+                from_email=contact_form.email,
+                recipient_list=[settings.EMAIL_HOST_USER],
+            )
 
             return redirect('/')
         else:
