b908e9ca9978040b1ae3f3f65a67b7d709f967b7
GenaPlem <shainkill1337@gmail.com>
2023-12-26 11:57:09 +0000

    Add urls for orders and shipping info

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('orders/', views.orders, name='orders'),
+    path('shipping_info/', views.shipping_info, name='shipping_info'),
 ]
