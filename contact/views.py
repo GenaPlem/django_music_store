4e82e00dbc516a87f62b09dbda00edcecba4ad75
GenaPlem <shainkill1337@gmail.com>
2023-12-26 17:03:04 +0000

    Update contact template

--- contact/views.py
+++ contact/views.py
@@ -4,13 +4,13 @@ from .models import Contact
 
 
 def contact(request):
-    form = None
+    form = ContactForm()
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            contact_form = form.save()  # This will save the data to the database
-            # Optionally send an email here if required
-            return redirect('thank_you')  # Redirect to a 'thank you' page
+            contact_form = form.save()
+
+            return redirect('/')
         else:
             form = ContactForm()
     return render(request, 'contact/contact.html', {'form': form})
