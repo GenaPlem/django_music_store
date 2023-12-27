16b2968aaf5cfeab42b61cca95a3a9f82756ee30
GenaPlem <shainkill1337@gmail.com>
2023-12-27 18:41:14 +0000

    Add views for bag app

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from products.models import Product
+from .utils import add_to_bag, remove_from_bag, update_bag
 
 
-def bag(request):
+def bag_view(request):
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag_view(request, product_id):
+    quantity = int(request.POST.get('quantity', 1))
+    product = get_object_or_404(Product, pk=product_id)
+    add_to_bag(request, product.id, quantity)
+    return redirect('bag')
+
+
+def remove_from_bag_view(request, product_id):
+    remove_from_bag(request, product_id)
+    return redirect('bag')
+
+
+def update_bag_view(request, product_id):
+    quantity = int(request.POST.get('quantity'))
+    update_bag(request, product_id, quantity)
+    return redirect('bag')
