23337905a4686670fac7b468acc0dca0416200b3
GenaPlem <shainkill1337@gmail.com>
2023-12-27 18:41:25 +0000

    Update urls for add products

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bag, name='bag'),
+    path('', views.bag_view, name='bag'),
+    path('add/<int:product_id>/', views.add_to_bag_view, name='add_to_bag'),
 ]
