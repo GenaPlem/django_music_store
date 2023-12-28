e5f3839c348081cae72ed3be8ee7794b804f9f44
GenaPlem <shainkill1337@gmail.com>
2023-12-28 13:45:03 +0000

    Update redirect after additing, removing and updating from/to bag

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from products.models import Product
 from .utils import add_to_bag, remove_from_bag, update_bag
@@ -11,15 +12,15 @@ def add_to_bag_view(request, product_id):
     quantity = int(request.POST.get('quantity', 1))
     product = get_object_or_404(Product, pk=product_id)
     add_to_bag(request, product.id, quantity)
-    return redirect('bag')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'bag_view'))
 
 
 def remove_from_bag_view(request, product_id):
     remove_from_bag(request, product_id)
-    return redirect('bag')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'bag_view'))
 
 
 def update_bag_view(request, product_id):
     quantity = int(request.POST.get('quantity'))
     update_bag(request, product_id, quantity)
-    return redirect('bag')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'bag_view'))
