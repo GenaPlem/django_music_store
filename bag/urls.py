fb3d7b2bd23dc0c1485f29e1a71fd309cb9034b0
GenaPlem <shainkill1337@gmail.com>
2023-12-28 13:16:00 +0000

    Add url to remove items from shopping bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.bag_view, name='bag'),
     path('add/<int:product_id>/', views.add_to_bag_view, name='add_to_bag'),
+    path('remove/<int:product_id>/', views.remove_from_bag_view, name='remove_from_bag')
 ]
