f1a4a5d93526f3291b261ac48675b89d9d7cba55
GenaPlem <shainkill1337@gmail.com>
2023-12-29 10:57:02 +0000

    Update add to bag function

--- bag/utils.py
+++ bag/utils.py
@@ -1,6 +1,20 @@
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
+
+
 def add_to_bag(request, product_id, quantity=1):
     bag = request.session.get('bag', {})
-    bag[str(product_id)] = bag.get(str(product_id), 0) + quantity
+    product = get_object_or_404(Product, pk=product_id)
+
+    current_quantity = bag.get(str(product_id), 0)
+    new_quantity = current_quantity + quantity
+
+    if new_quantity > product.quantity_in_stock:
+        bag[str(product_id)] = product.quantity_in_stock
+    else:
+        bag[str(product_id)] = new_quantity
+
     request.session['bag'] = bag
 
 
