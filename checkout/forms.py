30f8da002b9d05bae9dc4c30714cc15f3aebbb3e
GenaPlem <shainkill1337@gmail.com>
2024-01-02 15:26:02 +0000

    Add autofocus on first field in checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,3 +8,11 @@ class OrderForm(forms.ModelForm):
         fields = ['full_name', 'email', 'phone_number',
                   'address1', 'address2', 'city',
                   'county', 'postal_code']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
