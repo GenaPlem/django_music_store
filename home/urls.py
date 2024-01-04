094202825ee974edf91f2a0fb96fbdecc4367de2
GenaPlem <shainkill1337@gmail.com>
2024-01-04 15:27:02 +0000

    Add urls and views for newsletter

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, newsletter_subscribe
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('newsletter_subscribe/', newsletter_subscribe, name='newsletter_subscribe'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+
+from home.forms import NewsletterForm
+from home.models import NewsletterSubscriber
 from products.models import Product
 
 
@@ -6,3 +11,23 @@ def home_view(request):
     """ A view to return the index page """
     top_products = Product.objects.filter(is_top_product=True)[:6]
     return render(request, 'home/index.html', {'top_products': top_products})
+
+
+def newsletter_subscribe(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            if not NewsletterSubscriber.objects.filter(email=email).exists():
+                NewsletterSubscriber.objects.create(email=email)
+            send_mail(
+                'Subscription Confirmation',
+                'Thank you for subscribing to our newsletter!',
+                settings.EMAIL_HOST_USER,
+                [form.cleaned_data['email']],
+                fail_silently=False,
+            )
+            return redirect('home')
+        else:
+            form = NewsletterForm()
+    return render(request, 'home', {'form': form})
