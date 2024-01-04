6bfc86402ed95dabaaf79f015ee1ffbed496ab37
GenaPlem <shainkill1337@gmail.com>
2024-01-04 15:26:26 +0000

    Add Newsletter to show in admin's panel

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import NewsletterSubscriber
 
-# Register your models here.
+
+@admin.register(NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_subscribed', 'date_subscribed', 'date_unsubscribed')
+    list_filter = ('is_subscribed',)
+    search_fields = ('email',)
+    ordering = ('-date_subscribed',)
