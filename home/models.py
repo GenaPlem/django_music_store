7bc2a107134c943d56d259490ea4021b5e80c823
GenaPlem <shainkill1337@gmail.com>
2024-01-04 15:25:44 +0000

    Add NewsletterSubscriber model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterSubscriber(models.Model):
+    email = models.EmailField(unique=True)
+    is_subscribed = models.BooleanField(default=True)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+    date_unsubscribed = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email
