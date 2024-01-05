6548741a900708dc13c822325d90a71c65b403c0
GenaPlem <shainkill1337@gmail.com>
2024-01-05 12:06:22 +0000

    Update settings Debug

--- mtune/settings.py
+++ mtune/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') else False
 
 ALLOWED_HOSTS = ['m-tune-d4849222b5e5.herokuapp.com', '127.0.0.1']
 
