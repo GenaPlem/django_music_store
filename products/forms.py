44078efec5efaaa9f81af9285dd3f1c109bdea4f
GenaPlem <shainkill1337@gmail.com>
2024-01-07 12:57:13 +0000

    Add Product form for additing new products

---
+++ products/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Product
+from .widgets import CustomClearableFileInput
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
+
+        }
+
+    image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
