fdd0f5ae812cee7d8c42058a35fe98330248902f
GenaPlem <shainkill1337@gmail.com>
2024-01-08 13:36:13 +0000

    Fix alerts if there is no message to display

--- static/js/alerts.js
+++ static/js/alerts.js
@@ -1,10 +1,12 @@
 // Waits for DOM content to load before showing the 'alertBox' for 5 seconds.
 document.addEventListener('DOMContentLoaded', function () {
     const alert = document.getElementById('alertBox');
-    setTimeout(() => {
-        alert.classList.add('hide');
+    if (alert) {
         setTimeout(() => {
-            alert.remove();
-        }, 600);
-    }, 5000);
+            alert.classList.add('hide');
+            setTimeout(() => {
+                alert.remove();
+            }, 600);
+        }, 5000);
+    }
 });
