5ab8e621bd4d1175c3bd4ffcb384045f606e534b
GenaPlem <shainkill1337@gmail.com>
2024-01-08 11:48:14 +0000

    Update save for Order line item model

--- checkout/models.py
+++ checkout/models.py
@@ -68,7 +68,12 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        if self.product.discount_percentage:
+            discount = (self.product.price * self.product.discount_percentage / 100)
+            discounted_price = self.product.price - discount
+            self.lineitem_total = discounted_price * self.quantity
+        else:
+            self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
