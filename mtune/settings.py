d6121632bcdc2d9200a0669aa8e1fa7bae99a357
GenaPlem <shainkill1337@gmail.com>
2025-02-19 13:06:24 +0000

    Update smtp settings with sendgrid

--- mtune/settings.py
+++ mtune/settings.py
@@ -83,11 +83,12 @@ LOGIN_REDIRECT_URL = 'profile'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 ROOT_URLCONF = 'mtune.urls'
 
